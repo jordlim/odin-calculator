71d527d822b91f1d8eabd89b23e3322cf31702c3
Jordan Lim <80717189+jordlim@users.noreply.github.com>
2024-10-01 22:30:02 +0000

    Implement basic calculator layout

---
+++ calc.js
@@ -0,0 +1,14 @@
+
+
+function operate(operator, op1, op2) {
+    switch(operator) {
+        case "+":
+            break;
+        case "-":
+            break;
+        case "/":
+            break;
+        case "%":
+            break;
+    }
+}
