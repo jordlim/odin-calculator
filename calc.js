cc9c5ec04752dfd01ba43dc12bef9dc6c33eddab
Jordan Lim <80717189+jordlim@users.noreply.github.com>
2024-10-03 23:46:29 +0000

    Implement display update and clear

--- calc.js
+++ calc.js
@@ -1,3 +1,54 @@
+function calc() {
+    // Current display
+    let display = document.querySelector("#amount");
+
+    const buttons = document.querySelectorAll("button");
+
+    // *** ADD A BACKSPACE FEATURE ***
+
+    // 0 by default
+    let stack = [] 
+
+    buttons.forEach((btn) => {
+        btn.addEventListener("click", () => {
+        
+            let id = btn.id;
+            let text = btn.textContent;
+            // +, -, *, / (execute, clear stack, invert color)
+
+            // = (execute last op and number)
+
+            // % (divide by 100)
+
+            // 0
+
+            // 1-9
+            nums=["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+            if (nums.includes(id)){
+                if (stack.length == 0) {
+                    display.textContent = "";
+                }
+                stack.push(id);
+                display.textContent = display.textContent + text;
+            }
+
+            
+            
+                    
+            // .
+
+            // +/-
+
+            // C (clear and change to AC)
+            if (id == "clear") {
+                display.textContent = "0";
+                stack = [];
+            }
+            
+        });
+    });
+
+}
 
 
 function operate(operator, op1, op2) {
@@ -12,3 +63,9 @@ function operate(operator, op1, op2) {
             break;
     }
 }
+
+function update(val) {
+    
+}
+
+calc();
